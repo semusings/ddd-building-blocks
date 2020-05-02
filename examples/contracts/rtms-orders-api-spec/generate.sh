2b917dbe18c7b19e325d1fd70616b3fbad752604
Bhuwan Upadhyay <bhuwan.upadhyay@clusus.com>
2020-05-02 07:25:40 +0545

    feat: design ddd building blocks

--- examples/contracts/rtms-orders-api-spec/generate.sh
+++ examples/contracts/rtms-orders-api-spec/generate.sh
@@ -1,11 +1,8 @@
-# exit when any command fails
 set -e
 
 export SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
-cd "$SCRIPT_DIR"
-
-rm -rf out
+rm -rf "$SCRIPT_DIR"/out
 
 openapi-generator batch "$SCRIPT_DIR"/generators/spring-boot.yaml
 cd out/spring-boot
@@ -17,6 +14,4 @@ openapi-generator batch "$SCRIPT_DIR"/generators/http-client.yaml
 cd out/http-client
 mvn com.coveo:fmt-maven-plugin:format source:jar-no-fork install
 
-cd "$SCRIPT_DIR"
-
-rm -rf out
+rm -rf "$SCRIPT_DIR"/out
