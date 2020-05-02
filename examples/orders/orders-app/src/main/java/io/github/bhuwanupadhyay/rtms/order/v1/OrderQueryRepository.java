804a8fc8eb962074d3b920c160aa00e6ac9296a3
Bhuwan Upadhyay <bhuwan.upadhyay@clusus.com>
2020-05-02 12:34:08 +0545

    feat: design ddd building blocks

--- examples/orders/orders-app/src/main/java/io/github/bhuwanupadhyay/rtms/order/v1/OrderQueryRepository.java
+++ examples/orders/orders-app/src/main/java/io/github/bhuwanupadhyay/rtms/order/v1/OrderQueryRepository.java
@@ -21,47 +21,32 @@ class OrderQueryRepository {
 
   private final JdbcTemplate jdbc;
   private final OrderQueries queries;
+  private final OrderResourceMapper rowMapper;
 
   public OrderQueryRepository(JdbcTemplate jdbc, OrderQueries queries) {
     this.jdbc = jdbc;
     this.queries = queries;
+    this.rowMapper = new OrderResourceMapper();
   }
 
   public OrderResource findByOrderId(String orderId) {
     LOG.atInfo().log("Find order by order id %s", orderId);
-    return jdbc.query(
-        queries.getOrderById(),
-        new Object[] {orderId},
-        rs -> {
-          return Optional.of(rs.next())
-              .filter(Boolean::booleanValue)
-              .map(ignored -> toOrderResource(rs))
-              .orElseThrow(() -> new EntityNotFound("OrderIdDoesNotExists"));
-        });
+    return Optional.ofNullable(
+            jdbc.queryForObject(queries.getOrderById(), new Object[] {orderId}, rowMapper))
+        .orElseThrow(() -> new EntityNotFound("OrderIdDoesNotExists"));
   }
 
   public OrderPageList findAll(
       OrderResource filterResource, Integer pageSize, Integer pageNumber, String sort) {
     LOG.atInfo().log("Find all orders with page size %d, page number %d", pageSize, pageNumber);
-    final Long rowsCount =
-        jdbc.queryForObject(queries.getCountOrders(), new Object[] {}, Long.class);
+    final Object[] args = {};
+
+    final Long rowsCount = jdbc.queryForObject(queries.getCountOrders(), args, Long.class);
 
     int startRow = pageNumber * pageSize;
     int endRow = startRow + pageSize;
 
-    final List<OrderResource> orderResources =
-        jdbc.query(
-            queries.getOrders(),
-            new Object[] {},
-            new RowMapper<>() {
-              @Override
-              public OrderResource mapRow(ResultSet rs, int rowNum) throws SQLException {
-                return Optional.of(rs.next())
-                    .filter(Boolean::booleanValue)
-                    .map(ignored -> toOrderResource(rs))
-                    .orElseGet(() -> null);
-              }
-            });
+    final List<OrderResource> items = jdbc.query(queries.getOrders(), args, rowMapper);
 
     int totalPages = (int) (rowsCount / pageSize);
     if (rowsCount % pageSize != 0) {
@@ -76,20 +61,24 @@ class OrderQueryRepository {
                 .pageSize(pageSize)
                 .pageNumber(pageNumber)
                 .sort(sort))
-        .content(orderResources);
+        .content(items);
   }
 
-  private OrderResource toOrderResource(ResultSet rs) {
-    try {
-      return new OrderResource()
-          .id(rs.getString("order_id"))
-          .customerId(rs.getString("customer_id"))
-          .productId(rs.getString("product_id"))
-          .contactPhone(rs.getString("contact_phone"))
-          .deliveryAddress(rs.getString("delivery_address"))
-          .quantity(rs.getInt("quantity"));
-    } catch (SQLException e) {
-      throw new AppException.DataAccessException("ExceptionOccurredWhileMappingOrderResultSet", e);
+  private static final class OrderResourceMapper implements RowMapper<OrderResource> {
+
+    public OrderResource mapRow(ResultSet rs, int rowNum) throws SQLException {
+      try {
+        return new OrderResource()
+            .id(rs.getString("order_id"))
+            .customerId(rs.getString("customer_id"))
+            .productId(rs.getString("product_id"))
+            .contactPhone(rs.getString("contact_phone"))
+            .deliveryAddress(rs.getString("delivery_address"))
+            .quantity(rs.getInt("quantity"));
+      } catch (SQLException e) {
+        throw new AppException.DataAccessException(
+            "ExceptionOccurredWhileMappingOrderResultSet", e);
+      }
     }
   }
 }
